fe7f98b9f34a0cc84153ee73ec2fd7746e60a900
Bade Harish <badeharish@zuzucodes.com>
2026-03-04 01:26:04 +0530

    add try catch method on localstorage file

--- src/utils/localStorage.js
+++ src/utils/localStorage.js
@@ -1,18 +1,46 @@
+// export const safeLocalStorage = {
+//   getItem(key) {
+//     if (typeof window !== "undefined") {
+//       return localStorage.getItem(key);
+//     }
+//     return null;
+//   },
+//   setItem(key, value) {
+//     if (typeof window !== "undefined") {
+//       localStorage.setItem(key, value);
+//     }
+//   },
+//   removeItem(key) {
+//     if (typeof window !== "undefined") {
+//       localStorage.removeItem(key);
+//     }
+//   },
+// };
+
+
 export const safeLocalStorage = {
   getItem(key) {
-    if (typeof window !== "undefined") {
-      return localStorage.getItem(key);
+    try {
+      if (typeof window !== "undefined") {
+        return localStorage.getItem(key);
+      }
+      return null;
+    } catch (e) {
+      return null;
     }
-    return null;
   },
   setItem(key, value) {
-    if (typeof window !== "undefined") {
-      localStorage.setItem(key, value);
-    }
+    try {
+      if (typeof window !== "undefined") {
+        localStorage.setItem(key, value);
+      }
+    } catch (e) {}
   },
   removeItem(key) {
-    if (typeof window !== "undefined") {
-      localStorage.removeItem(key);
-    }
+    try {
+      if (typeof window !== "undefined") {
+        localStorage.removeItem(key);
+      }
+    } catch (e) {}
   },
 };
