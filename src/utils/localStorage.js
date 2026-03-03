8cba9cc73c79fc883fc029dd292a6b706dcda2bc
Bade Harish <badeharish@zuzucodes.com>
2026-03-04 01:32:45 +0530

    putting the exist code on localstorage file

--- src/utils/localStorage.js
+++ src/utils/localStorage.js
@@ -1,46 +1,18 @@
-// export const safeLocalStorage = {
-//   getItem(key) {
-//     if (typeof window !== "undefined") {
-//       return localStorage.getItem(key);
-//     }
-//     return null;
-//   },
-//   setItem(key, value) {
-//     if (typeof window !== "undefined") {
-//       localStorage.setItem(key, value);
-//     }
-//   },
-//   removeItem(key) {
-//     if (typeof window !== "undefined") {
-//       localStorage.removeItem(key);
-//     }
-//   },
-// };
-
-
 export const safeLocalStorage = {
   getItem(key) {
-    try {
-      if (typeof window !== "undefined") {
-        return localStorage.getItem(key);
-      }
-      return null;
-    } catch (e) {
-      return null;
+    if (typeof window !== "undefined") {
+      return localStorage.getItem(key);
     }
+    return null;
   },
   setItem(key, value) {
-    try {
-      if (typeof window !== "undefined") {
-        localStorage.setItem(key, value);
-      }
-    } catch (e) {}
+    if (typeof window !== "undefined") {
+      localStorage.setItem(key, value);
+    }
   },
   removeItem(key) {
-    try {
-      if (typeof window !== "undefined") {
-        localStorage.removeItem(key);
-      }
-    } catch (e) {}
+    if (typeof window !== "undefined") {
+      localStorage.removeItem(key);
+    }
   },
 };
